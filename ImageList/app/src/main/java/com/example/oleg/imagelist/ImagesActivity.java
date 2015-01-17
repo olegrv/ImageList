e6e61854c13786c375788e63fecd9793fc2c3327
Oleg Ryabov <oleg.wr@gmail.com>
2015-01-17 22:40:57 +0300

    useless menu was removed

--- ImageList/app/src/main/java/com/example/oleg/imagelist/ImagesActivity.java
+++ ImageList/app/src/main/java/com/example/oleg/imagelist/ImagesActivity.java
@@ -2,8 +2,6 @@ package com.example.oleg.imagelist;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class ImagesActivity extends ActionBarActivity {
@@ -17,29 +15,6 @@ public class ImagesActivity extends ActionBarActivity {
 
         m_grid = (MyCustomGrid) findViewById(R.id.mg);
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_images, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
